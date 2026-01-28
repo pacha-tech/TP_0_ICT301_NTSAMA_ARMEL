94c17ac4f9a9dfdde0b0fd929d0aeefbea4f026d
pacha-tech <armel8594@gmail.com>
2026-01-28 10:58:46 +0100

    deplacement du fichier pdf

--- src/main/java/org/example/OCP/avant/Main1.java
+++ src/main/java/org/example/OCP/avant/Main1.java
@@ -4,8 +4,8 @@ public class Main1 {
     public static void main(String[] args) {
         AreaCalculator calculator = new AreaCalculator();
 
-        Object monRectangle = new Rectangle();
-        Object monCercle = new Cercle();
+        Rectangle monRectangle = new Rectangle();
+        Cercle monCercle = new Cercle();
 
         System.out.println("--- AVANT OCP ---");
         System.out.println("Aire Rectangle : " + calculator.calculateArea(monRectangle));
